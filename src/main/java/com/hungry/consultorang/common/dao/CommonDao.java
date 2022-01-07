d02cb00b98eba51d5908caafd2a1a3528a8e24c8
Seongjunsim <sjsim0319@gmail.com>
2022-01-07 21:47:20 +0900

    spring boot rds 연동 완료

--- src/main/java/com/hungry/consultorang/common/dao/CommonDao.java
+++ src/main/java/com/hungry/consultorang/common/dao/CommonDao.java
@@ -1,4 +1,23 @@
 package com.hungry.consultorang.common.dao;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.List;
+
+@Repository
 public class CommonDao {
+
+    private SqlSession sqlSession;
+
+    public CommonDao(SqlSession sqlSession) {
+        this.sqlSession = sqlSession;
+    }
+
+    public List<HashMap<String, Object>> getUser(){
+        return sqlSession.selectList("common.getUserList");
+    }
+
+
 }
--- src/main/java/com/hungry/consultorang/controller/TestController.java
+++ src/main/java/com/hungry/consultorang/controller/TestController.java
@@ -1,18 +1,31 @@
 package com.hungry.consultorang.controller;
 
+import com.hungry.consultorang.common.dao.CommonDao;
 import com.hungry.consultorang.model.TestModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.HashMap;
+import java.util.List;
+
 
 @Controller
 public class TestController {
 
+    @Autowired
+    CommonDao commonDao;
+
     @GetMapping("/hello")
     public ResponseEntity<TestModel> helloWorld(){
         return new ResponseEntity<TestModel>(new TestModel(10, "sim"), HttpStatus.OK);
+    }
+
+    @GetMapping("/hi")
+    public ResponseEntity<List<HashMap<String, Object>>> hiWorld(){
+        return new ResponseEntity<List<HashMap<String, Object>>>(commonDao.getUser(), HttpStatus.OK);
     }
 }
