4d9d27d17097c00714aad1d85361ee1a1bba4205
Jen <wonee0321@gmail.com>
2022-01-27 02:39:03 +0900

    [ADD] #16 SurveyRequest, ResponseModel 추가

---
+++ src/main/java/com/hungry/consultorang/model/login/SurveyRequestModel.java
@@ -0,0 +1,10 @@
+package com.hungry.consultorang.model.login;
+
+import com.hungry.consultorang.model.ParentModel;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class SurveyRequestModel extends ParentModel {
+}
---
+++ src/main/java/com/hungry/consultorang/model/login/SurveyResponseModel.java
@@ -0,0 +1,12 @@
+package com.hungry.consultorang.model.login;
+
+import com.hungry.consultorang.model.ParentModel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Builder
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class SurveyResponseModel extends ParentModel {
+}
